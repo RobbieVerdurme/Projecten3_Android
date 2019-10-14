751689d6e5b3c7ac3e53707bdc595d0f4a39feaa
rubengrillaert <ruben.grillaert.y1033@student.hogent.be>
2019-10-14 15:19:01 +0200

    Layout login

--- app/src/main/java/be/multinet/ui/fragment/LoginFragment.kt
+++ app/src/main/java/be/multinet/ui/fragment/LoginFragment.kt
@@ -7,18 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import be.multinet.R
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * The login [Fragment] that lets users enter the rest of the app.
  */
 class LoginFragment : Fragment() {
-
     /**
      * Set up the layout.
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //TODO use a DataBinding class (LoginFragmentBinding) to inflate and setup lifecycleowner + viewmodel etc
+
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
