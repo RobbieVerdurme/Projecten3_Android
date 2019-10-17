4ffb9c7c6990c2d73fbe8334542932b322ce3324
navaronbracke <brackenavaron@gmail.com>
2019-10-17 22:55:51 +0200

    Added basic chart to homeFragment.

--- app/src/main/java/be/multinet/ui/fragment/HomeFragment.kt
+++ app/src/main/java/be/multinet/ui/fragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package be.multinet.ui.fragment
 
+import android.graphics.Color
 import be.multinet.databinding.FragmentHomeBinding
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -11,14 +12,23 @@ import androidx.lifecycle.ViewModelProviders
 
 import be.multinet.R
 import be.multinet.viewmodel.HomeViewModel
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * This [Fragment] represents the home page.
  */
 class HomeFragment : Fragment() {
 
+    /**
+     * The [HomeViewModel] for this fragment.
+     */
+    lateinit var viewModel: HomeViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val binding = FragmentHomeBinding.inflate(inflater,container,false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -36,6 +46,39 @@ class HomeFragment : Fragment() {
     private fun setupFragment() {
         val toolbar = (activity as AppCompatActivity).supportActionBar!!
         toolbar.title = getString(R.string.home_title)
+        setupChart()
+    }
+
+    /**
+     * Setup the progression chart
+     */
+    private fun setupChart(){
+        chart.description.isEnabled = false
+        chart.xAxis.setDrawLabels(false)
+        //Set the data
+        val points: LineDataSet
+        //If there is a dataset, update it
+        if(chart.data != null && chart.data.dataSetCount > 0){
+            points = chart.data.getDataSetByIndex(0) as LineDataSet
+            points.values = viewModel.getChartData()
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+        }
+        else{
+            //Otherwise create a new one
+            points = LineDataSet(viewModel.getChartData(),viewModel.chartLabel)
+            points.setDrawValues(false)
+            points.color = Color.BLUE
+            points.lineWidth = 2f
+            points.formLineWidth = 1f
+            points.formSize = 8f
+            points.fillColor = Color.LTGRAY
+
+            val dataset = ArrayList<ILineDataSet>()
+            dataset.add(points)
+            val lineData = LineData(dataset)
+            chart.data = lineData
+        }
     }
 
 
