fb7dc6af48e0e0007867e72c7e1617c1e3b3edb1
navaronbracke <brackenavaron@gmail.com>
2019-12-11 00:40:45 +0100

    Updated ChallengeItem layout. Added Complete challenge land layout. Added new ChallengeAdapter.

--- app/src/main/java/be/multinet/adapter/IChallengeAdapter.kt
+++
@@ -1,9 +0,0 @@
-package be.multinet.adapter
-
-import androidx.cardview.widget.CardView
-
-interface IChallengeAdapter {
-    fun getBaseElevation(): Float
-    fun getCardViewAt(position: Int): CardView
-    fun getCount() : Int
-}
