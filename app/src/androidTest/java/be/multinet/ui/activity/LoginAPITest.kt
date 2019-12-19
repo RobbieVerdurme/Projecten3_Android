47922702ce37c3c8b7caec28e98e76eafdcd3767
navaronbracke <brackenavaron@gmail.com>
2019-12-19 15:35:49 +0100

    Added Login IOExceptionTest

--- app/src/androidTest/java/be/multinet/ui/activity/LoginAPITest.kt
+++ app/src/androidTest/java/be/multinet/ui/activity/LoginAPITest.kt
@@ -1,48 +1,32 @@
 package be.multinet.ui.activity
 
-import androidx.room.Room
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import org.koin.core.context.loadKoinModules
-import org.koin.core.context.unloadKoinModules
-import org.koin.dsl.module
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnitRunner
 import be.multinet.R
-import be.multinet.database.ApplicationDatabase
-import be.multinet.matchers.hasTextInputLayoutErrorMessage
 import be.multinet.model.User
 import be.multinet.network.NetworkHandler
-import be.multinet.repository.*
-import be.multinet.repository.Interface.IChallengeRepository
+import be.multinet.repository.DataError
+import be.multinet.repository.DataOrError
 import be.multinet.repository.Interface.ILeaderboardUserRepoitory
-import be.multinet.repository.Interface.ITherapistRepository
 import be.multinet.repository.Interface.IUserRepository
 import be.multinet.runner.MultinetTestApp
-import be.multinet.viewmodel.HomeViewModel
 import be.multinet.viewmodel.LoginViewModel
 import be.multinet.viewmodel.UserViewModel
 import io.mockk.coEvery
 import io.mockk.mockk
-import org.hamcrest.Matchers
-import org.hamcrest.Matchers.allOf
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
+import org.hamcrest.Matchers.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.koinApplication
+import org.koin.dsl.module
 import org.koin.test.KoinTest
-import org.koin.test.check.checkModules
 import java.util.*
 
 
@@ -105,6 +89,56 @@ class LoginAPITest : KoinTest {
                     .check(matches(isDisplayed()))
 
                 onView(withText(R.string.dialog_cancel)).perform(click())
+            }
+        }
+    }
+
+    @Test
+    fun loginIOExceptionHandlesError(){
+        val userRepoMock: IUserRepository = mockk()
+        val leaderboardRepoMock : ILeaderboardUserRepoitory = mockk()
+
+        val username = "username"
+        val password = "password"
+        val contract = Date()
+        val token = "token"
+        val user = User("1",token,"name","familyName","mail","phone",contract, listOf(),0)
+
+
+        //declare module
+        val module = module {
+            viewModel {
+                UserViewModel(get())
+            }
+            viewModel {
+                LoginViewModel(get(),get())
+            }
+            single {
+                userRepoMock
+            }
+            single {
+                leaderboardRepoMock
+            }
+        }
+
+
+        //load module
+        app.loadModules(module){
+            //train mock
+            coEvery { userRepoMock.loadApplicationUser() } coAnswers { DataOrError(data = null) }
+            coEvery {userRepoMock.login(eq(username),eq(password))} coAnswers { DataOrError(error = DataError.API_SERVER_UNREACHABLE,data = null) }
+            coEvery {leaderboardRepoMock.loadLeaderboard(eq(token),eq(user.getUserId().toInt()))} coAnswers { DataOrError(data = listOf()) }
+
+            //launch
+            val scenario = ActivityScenario.launch(MainActivity::class.java)
+            scenario.use {
+                NetworkHandler.onNetworkAvailable()
+
+                val usernameInput = onView(withId(R.id.usernameInput))
+                usernameInput.perform(typeText("username")).perform(closeSoftKeyboard())
+                val passwordInput = onView(withId(R.id.passwordInput))
+                passwordInput.perform(typeText("password")).perform(closeSoftKeyboard())
+                onView(allOf(withId(R.id.login), withText(R.string.login_title))).perform(click())
             }
         }
     }
